e2a374d8b9b1da75a81c4e1124106bfa4238a40a
Luan Albert <luan.albert@mercadolivre.com>
2021-12-27 18:50:59 -0300

    Conversor de Decimais(Entendido :D)

---
+++ src/main/java/com/meli/conversordecimais/ConversorDecimais.java
@@ -0,0 +1,32 @@
+package com.meli.conversordecimais;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static java.lang.String.join;
+import static java.util.Collections.nCopies;
+
+@RestController
+@RequestMapping("numero")
+public class ConversorDecimais {
+    private static final int[] decimais = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] romanos = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    @GetMapping(path = "/{numero}")
+    public String numeroRomano(@PathVariable("numero") int numero) {
+
+        StringBuilder resultado = new StringBuilder();
+
+        for (int i = 0; i < decimais.length; i++) {
+
+            int parteInteira = numero / decimais[i];
+            numero -= decimais[i] * parteInteira;
+            resultado.append(join("", nCopies(parteInteira, romanos[i])));
+        }
+        return "Em números romanos é " + resultado;
+
+    }
+
+}
---
+++ src/main/java/com/meli/conversordecimais/ConversorDecimaisApplication.java
@@ -0,0 +1,13 @@
+package com.meli.conversordecimais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorDecimaisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConversorDecimaisApplication.class, args);
+    }
+
+}
